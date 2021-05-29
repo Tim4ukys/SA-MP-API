67c660f8e6caea8d6d4aea9ce7b794a9921a9f48
Tim4ukys <tim4ukys@yandex.ru>
2021-05-29 20:08:19 +0700

    last update for sa-mp r3
    Обновил все структуры и оффсеты... Осталось только тестить и фиксить баги.

---
+++ SA-MP API/samp/0_3_7-R3/stGameInfo.lua
@@ -0,0 +1,59 @@
+--[[
+	Project: unnamed multihack
+
+	Author: LUCHARE
+	
+
+	All structures are taken from mod_s0beit_sa.
+	Copyright: BlastHack
+	mod_sa is available from https://github.com/BlastHackNet/mod_s0beit_sa/
+]]
+
+local sys = require 'SA-MP API.kernel'
+
+sys.safely_include 'SA-MP API.samp.0_3_7-R3.stSAMPPed'
+
+sys.ffi.cdef[[
+	struct stAudio
+	{
+		int	iSoundState; // -- 0 - Finished, 1 - Loaded, 2 - Playing
+		bool m_bOutdoorAmbienceTrackDisabled;
+	}__attribute__ ((packed));
+
+	struct stCamera
+	{
+		void*	pEntity; // -- mabe m_pAttachedTo
+		struct CMatrix_Padded* matrix;
+	}__attribute__ ((packed));
+
+	struct stGameInfo
+	{
+		struct stAudio*	pAudio;
+		struct stCamera*	pCamera;
+		stSAMPPed*	pLocalPlayerPed;
+		
+		// -- stracingCheckpoint | not tested
+		float		fRaceCheckpointPos[3];
+		float		fRaceCheckpointNext[3];
+		float		m_fRaceCheckpointSize;
+		uint8_t		byteRaceType;
+		int			bRaceCheckpointsEnabled;
+		unsigned long		dwRaceCheckpointMarker;
+		unsigned long		dwRaceCheckpointHandle;
+
+		// -- stCheckpoint | not tested
+		float			fCheckpointPos[3];
+		float			fCheckpointExtent[3];
+		int				bCheckpointsEnabled;
+		unsigned long	dwCheckpointMarker;
+		
+		uint32_t 	ulUnk1;
+		int 		bHeadMove;
+		int 		nFrameLimit;
+		int 		iCursorMode;
+		int 		ulFpsLimit;
+		int 		bClockEnabled;
+		uint8_t 	byteUnk3;
+		bool    	byteVehicleModels[212];
+	}__attribute__ ((packed));
+]]
---
+++ SA-MP API/samp/0_3_7-R3/stRemotePlayer.lua
@@ -0,0 +1,72 @@
+--[[
+	Project: SA-MP API
+
+	Author: LUCHARE
+	
+
+	All structures are taken from mod_s0beit_sa.
+	Copyright: BlastHack
+	mod_sa is available from https://github.com/BlastHackNet/mod_s0beit_sa/
+]]
+
+local sys = require 'SA-MP API.kernel'
+
+sys.ffi.cdef[[
+	struct stRemotePlayerData {
+		struct stSAMPPed		*pSAMP_Actor;
+		struct stSAMPVehicle	*pSAMP_Vehicle;
+		unsigned short				sPlayerID;
+		unsigned short				sVehicleID;
+		unsigned int			ulUnk5;
+		int						iShowNameTag;
+		int						iHasJetPack;
+		unsigned char					byteSpecialAction;
+
+		struct stInCarData		inCarData;
+		struct stTrailerData	trailerData;
+		struct stAimData		aimData;
+		struct stPassengerData	passengerData;
+		struct stOnFootData		onFootData;
+
+		unsigned char					byteTeamID;
+		unsigned char					bytePlayerState;
+		unsigned char					byteSeatID;
+		unsigned int				ulUnk3;
+		int						iPassengerDriveBy;
+		
+		float					fOnFootPos[3];
+		float					fOnFootMoveSpeed[3];
+		float					fVehiclePosition[3];
+		float					fVehicleMoveSpeed[3];
+		unsigned char				pad_1[76];
+		float					fSomething[3];
+
+		// -- stincarTargetRotation
+		float 					fVehicleReal;
+		float 					fVehicleRoll[3];
+
+		float					fActorArmor;
+		float					fActorHealth;
+		unsigned char          pad_2[12];
+		unsigned int				ulUnk10;
+		unsigned char					byteUnk9;
+		unsigned int				dwTick;
+		unsigned int				dwLastStreamedInTick;
+		unsigned int				ulUnk7;
+		int						iAFKState;
+		struct stHeadSync		headSyncData;
+
+		int						iGlobalMarkerLoaded;
+		int						iGlobalMarkerLocation[3];
+		unsigned int				ulGlobalMarker_GTAID;
+	}__attribute__ ((packed));
+
+	struct stRemotePlayer {
+		stRemotePlayerData	*pPlayerData;
+		int					iPing;
+		void						*pVTBL_txtHandler;
+		char				strPlayerName[24];
+		int						iScore;
+		int						iIsNPC;
+	}__attribute__ ((packed));
+]]
